06075edd4f7895699a884c4682f67719261bfb0a
Zoltán Gyebrovszki <kavics@sensenet.com>
2020-02-03 10:24:09 +0100

    Edwin

--- src/netcore/SnWebApplicationWithIdentity/SnWebApplicationWithExternalIS4/Controllers/HomeController.cs
+++ src/netcore/SnWebApplicationWithIdentity/SnWebApplicationWithExternalIS4/Controllers/HomeController.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SenseNet.ContentRepository;
+using SenseNet.ContentRepository.Storage;
+using SenseNet.ContentRepository.Storage.Security;
 using SnWebApplicationWithExternalIS4.Models;
+using SnContent = SenseNet.ContentRepository.Content;
 
 namespace SnWebApplicationWithExternalIS4.Controllers
 {
@@ -39,6 +43,34 @@ namespace SnWebApplicationWithExternalIS4.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult HelloEdwin()
+        {
+            var name = "edwin";
+
+            using (new SystemAccount())
+            {
+                var parentPath = "/Root/IMS/BuiltIn/Temp";
+                if (!Node.Exists($"{parentPath}/{name}"))
+                {
+                    var parent = RepositoryTools.CreateStructure(parentPath, "OrganizationalUnit")
+                                 ?? SnContent.Load(parentPath);
+
+                    var user = new User(parent.ContentHandler)
+                    {
+                        Name = name,
+                        LoginName = name,
+                        Password = name,
+                        Email = $"{name}@example.com"
+                    };
+                    user.Save();
+                }
+            }
+
+            ViewData["Name"] = name;
+            ViewData["Email"] = $"{name}@example.com";
+            return View();
         }
     }
 }
