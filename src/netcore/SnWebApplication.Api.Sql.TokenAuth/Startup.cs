7e8aa90a0e414b76ef1375d1b0002c87a92b4482
tusmester <tusmester@gmail.com>
2020-03-03 13:40:48 +0100

    Add SQL-specific asp.net core template and installer.

---
+++ src/netcore/SnWebApplication.Api.Sql.TokenAuth/Startup.cs
@@ -0,0 +1,120 @@
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SenseNet.Configuration;
+using SenseNet.ContentRepository;
+using SenseNet.ContentRepository.Security;
+using SenseNet.ContentRepository.Storage.Data.MsSqlClient;
+using SenseNet.Diagnostics;
+using SenseNet.IdentityServer4.WebClient;
+using SenseNet.OData;
+using SenseNet.Search.Lucene29;
+using SenseNet.Security.EFCSecurityStore;
+using SenseNet.Services.Core;
+using SenseNet.Services.Core.Cors;
+using SenseNet.Services.Core.Virtualization;
+
+namespace SnWebApplication.Api.Sql.TokenAuth
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+
+            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+            // [sensenet]: Authentication
+            // Configure token authentication and add cookies so that non-script requests
+            // (e.g. downloading files and images) work too.
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = Configuration["sensenet:authentication:authority"];
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+
+                    options.Audience = "sensenet";
+                })
+                .AddDefaultSenseNetIdentityServerClients(Configuration["sensenet:authentication:authority"]);
+
+            //UNDONE: cookie auth is missing
+
+            // [sensenet]: add allowed client SPA urls
+            services.AddSenseNetCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            // [sensenet]: custom CORS policy
+            app.UseSenseNetCors();
+            // [sensenet]: use Authentication and set User.Current
+            app.UseSenseNetAuthentication();
+
+            app.UseAuthorization();
+
+            app.Use(async (context, next) =>
+            {
+                var user = User.Current.Name;
+
+                if (next != null)
+                    await next();
+            });
+
+            // [sensenet] Add the sensenet binary handler
+            app.UseSenseNetFiles();
+
+            // [sensenet]: OData middleware
+            app.UseSenseNetOdata();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+
+        internal static RepositoryBuilder GetRepositoryBuilder(IConfiguration configuration, string currentDirectory)
+        {
+            // assemble a SQL-specific repository
+
+            var repositoryBuilder = new RepositoryBuilder()
+                .UseConfiguration(configuration)
+                .UseLogger(new SnFileSystemEventLogger())
+                .UseTracer(new SnFileSystemTracer())
+                .UseAccessProvider(new UserAccessProvider())
+                .UseDataProvider(new MsSqlDataProvider())
+                .UseSecurityDataProvider(new EFCSecurityDataProvider(connectionString: ConnectionStrings.ConnectionString))
+                .UseLucene29LocalSearchEngine($"{currentDirectory}\\App_Data\\LocalIndex")
+                .StartWorkflowEngine(false)
+                .DisableNodeObservers()
+                .UseTraceCategories("Event", "Custom", "System") as RepositoryBuilder;
+
+            Providers.Instance.PropertyCollector = new EventPropertyCollector();
+
+            return repositoryBuilder;
+        }
+    }
+}
