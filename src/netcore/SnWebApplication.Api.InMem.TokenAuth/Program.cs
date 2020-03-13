92198e2e5f875fddc8a82875988f460a5db891b7
tusmester <tusmester@gmail.com>
2020-03-13 09:01:05 +0100

    Remove temp import from inmem api template.

--- src/netcore/SnWebApplication.Api.InMem.TokenAuth/Program.cs
+++ src/netcore/SnWebApplication.Api.InMem.TokenAuth/Program.cs
@@ -1,17 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using SenseNet.ContentRepository;
 using SenseNet.ContentRepository.InMemory;
 using SenseNet.ContentRepository.Security;
-using SenseNet.ContentRepository.Storage.Security;
-using SenseNet.Diagnostics;
-using SenseNet.Packaging;
 
 namespace SnWebApplication.Api.InMem.TokenAuth
 {
@@ -28,33 +19,6 @@ namespace SnWebApplication.Api.InMem.TokenAuth
             }))
             {
                 // put repository initialization here (e.g. import)
-
-                //UNDONE: remove temp content
-                // create a temp user and make it admin
-                using (new SystemAccount())
-                {
-                    var user = new User(User.Administrator.Parent)
-                    {
-                        Name = "edvin",
-                        LoginName = "edvin",
-                        Password = "edvin",
-                        Email = "edvin@example.com"
-                    };
-                    user.Save();
-                    Group.Administrators.AddMember(user);
-
-                    var parent = RepositoryTools.CreateStructure("/Root/MyContent", "SystemFolder");
-                    var docLib = RepositoryTools.CreateStructure("/Root/MyContent/MyFiles", "DocumentLibrary");
-
-                    ((GenericContent)docLib.ContentHandler).AllowChildType("Image", save: true);
-
-                    var file = new File(docLib.ContentHandler) { Name = "testfile.txt" };
-                    file.Binary.SetStream(RepositoryTools.GetStreamFromString($"temp text data {DateTime.UtcNow}"));
-                    file.Save();
-
-                    var installer = new Installer();
-                    installer.Import("C:\\temp\\import\\Root");
-                }
 
                 host.Run();
             }
