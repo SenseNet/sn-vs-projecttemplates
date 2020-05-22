028c76ceee6423051e4cd4252cfc1ea1a02779b0
vargajoe <varga.yosh@gmail.com>
2020-05-22 18:44:57 +0200

    show PII in development mode

--- src/netcore/SnWebApplication.Api.Sql.SearchService.TokenAuth/Startup.cs
+++ src/netcore/SnWebApplication.Api.Sql.SearchService.TokenAuth/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Logging;
 using SenseNet.Configuration;
 using SenseNet.ContentRepository;
 using SenseNet.ContentRepository.Security;
@@ -67,6 +68,7 @@ namespace SnWebApplication.Api.Sql.SearchService.TokenAuth
         {
             if (env.IsDevelopment())
             {
+                IdentityModelEventSource.ShowPII = true;
                 app.UseDeveloperExceptionPage();
             }
 
