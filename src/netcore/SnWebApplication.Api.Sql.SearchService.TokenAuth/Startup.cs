a8d7263e3c84e235e990c50020854e050481722e
vargajoe <varga.yosh@gmail.com>
2020-05-27 08:50:03 +0200

    fix TokenValidationParameters (api.sql.searchsrv.tokenauth)

--- src/netcore/SnWebApplication.Api.Sql.SearchService.TokenAuth/Startup.cs
+++ src/netcore/SnWebApplication.Api.Sql.SearchService.TokenAuth/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
 using SenseNet.Configuration;
 using SenseNet.ContentRepository;
 using SenseNet.ContentRepository.Security;
