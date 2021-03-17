78d5bd1048247c6897db58225446a223af107617
tusmester <tusmester@gmail.com>
2021-03-17 10:09:36 +0100

    Add MVC sample web app.

---
+++ src/netcore/SnWebApplication.Mvc.Client/Models/SnContent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SenseNet.Client;
+
+namespace SnWebApplication.Mvc.Client.Models
+{
+    public class SnContent
+    {
+        public dynamic Content { get; set; }
+        public IEnumerable<dynamic> Children { get; set; }
+    }
+}
