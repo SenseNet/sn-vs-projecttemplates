8ad339de82999e4d273a27a56e61289fd5f8a836
tusmester <tusmester@gmail.com>
2021-10-06 14:32:54 +0200

    Feature/mvc serverclient (#33)
    * Add MVC sample web app.
    
    * Add console client solution filter.

---
+++ src/netcore/SnWebApplication.Mvc.Client/Controllers/ContentController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SenseNet.Client;
+using SenseNet.Client.Authentication;
+using SnWebApplication.Mvc.Client.Models;
+
+namespace SnWebApplication.Mvc.Client.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly ITokenStore _tokenStore;
+
+        public ContentController(ITokenStore tokenStore)
+        {
+            _tokenStore = tokenStore;
+        }
+
+        public async Task<IActionResult> Index(int id = 0)
+        {
+            Content content;
+
+            var server = await GetSnServer();
+
+            if (id == 0)
+            {
+                // display the root
+                content = await SenseNet.Client.Content.LoadAsync("/Root/Content/SampleWorkspace", server);
+            }
+            else
+            {
+                // load the current content
+                content = await SenseNet.Client.Content.LoadAsync(id, server);
+            }
+
+            var children = await SenseNet.Client.Content.LoadCollectionAsync(content.Path, server);
+            
+            return View(new SnContent
+            {
+                Content = content,
+                Children = children
+            });
+        }
+
+        private async Task<ServerContext> GetSnServer()
+        {
+            // define sensenet service url
+            var server = new ServerContext
+            {
+                Url = "https://example.sensenet.cloud"
+            };
+
+            // request and set the access token
+            server.Authentication.AccessToken = await _tokenStore.GetTokenAsync(server,
+                "",
+                "");
+
+            return server;
+        }
+    }
+}
